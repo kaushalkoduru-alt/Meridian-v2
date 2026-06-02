05dbacb990a548c4c22a222007e63e940a99e7a8
Kaushal Koduru <kaushalkoduru@gmail.com>
2026-06-01 22:44:09 -0400

    add Clerk auth + Stripe Paywall modal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse, HTMLResponse, StreamingResponse
 import pandas as pd
